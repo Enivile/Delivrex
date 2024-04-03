1f2360bdfcace1bf503b54b1e53f34c2d6c5a4c8
ENIVILE <ahmed.ehtesam.zafar@gmail.com>
2024-04-04 02:21:57 +0500

    V1.0

---
+++ include/head.php
@@ -0,0 +1,37 @@
+
+        <!--====== Required meta tags ======-->
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <!-- Include the ScrollMagic library -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.8/ScrollMagic.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.8/plugins/debug.addIndicators.min.js"></script>
+        <!--====== Title ======-->
+        <title>Same Day Delivery - Deliverx</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <!--====== Favicon Icon ======-->
+        <link rel="shortcut icon" href="assets/images/favicon.ico" type="image/png">
+        <!--====== Bootstrap css ======-->
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+        <!--====== Flaticon css ======-->
+        <link rel="stylesheet" href="assets/fonts/flaticon/flaticon.css">
+        <!--====== Iconfont css ======-->
+        <link rel="stylesheet" href="assets/fonts/icofont/icofont.css">
+        <!--====== Gilory css ======-->
+        <link rel="stylesheet" href="assets/fonts/gilory/gilory.css">
+        <!--====== Magnific Popup css ======-->
+        <link rel="stylesheet" href="assets/css/magnific-popup.css">
+        <!--====== Slick css ======-->
+        <link rel="stylesheet" href="assets/css/slick.css">
+        <!--====== nice-select css ======-->
+        <link rel="stylesheet" href="assets/css/nice-select.css">
+        <!--====== Default css ======-->
+        <link rel="stylesheet" href="assets/css/default.css">
+        <!--====== Style css ======-->
+        <link rel="stylesheet" href="assets/css/style.css">
+        <!--====== Google Fonts ======-->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Michroma">
+
---
+++ include/sections/banner.php
@@ -0,0 +1,19 @@
+<section class="banner-area-v1">
+            <div class="hero-slider-one">
+                <div class="single-hero bg_cover" style="background-color: #161e25;">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-7">
+                                <div class="hero-content">
+                                    <span>Welcome to Delivrex</span>
+                                    <h1>Get Your Load to Market Faster.</h1>
+                                    <h4>The best civil engineering company</h4>
+                                    <a href="#" class="main-btn">Exploring More</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="hero-arrows"></div>
+        </section>
